8e5643d3ae26cba05e0c0ad1014b1c982805cf97
Mr Cookie <126540483+mrcookee@users.noreply.github.com>
2025-03-30 02:22:04 +0530

    Add files via upload

---
+++ assets/js/contactus.js
@@ -0,0 +1,30 @@
+function msgsent(event) {
+    event.preventDefault();  // Prevents the form from submitting and refreshing the page
+
+    var data = {
+        fbname: document.getElementById("fbname").value,
+        fbemail: document.getElementById("fbemail").value,
+        fbsubject: document.getElementById("fbsubject").value,
+        fbmsg: document.getElementById("fbmsg").value
+    };
+    emailjs.init({
+        publicKey: "uhRMmI0n4VXiWleNR",
+      });
+    emailjs.send("service_9yieeys", "template_z66xjwc", data).then(
+        res => {
+            // Reset form fields after success
+            document.getElementById("fbname").value = "";
+            document.getElementById("fbemail").value = "";
+            document.getElementById("fbmsg").value = "";
+            console.log(res);
+            document.getElementById("sent_message").style.opacity = "100%";
+        }
+    )
+        .catch((err) => {
+            console.log(err);
+            document.getElementById("sent_message").innerHTML = "we have faced some issues, please contact us via email.".style.opacity = "100%";
+        });
+    setTimeout(function () {
+        location.reload();
+    }, 5000);
+}
